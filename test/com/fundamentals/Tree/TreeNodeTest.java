a958205384d88fbe18f267f8adb55a0e82d6d79e
Rahul <sinha.r@northeastern.edu>
2020-10-10 17:28:47 -0400

    Updated tests for Binary Search Tree

--- test/com/fundamentals/Tree/TreeNodeTest.java
+++ test/com/fundamentals/Tree/TreeNodeTest.java
@@ -3,24 +3,50 @@ package com.fundamentals.Tree;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 class TreeNodeTest {
 
     @Test
+    //Test to create a tree and insert elements and do different types of traversals on the tree
     public void TreeTest()
     {
         TreeNode tree = new TreeNode(15);
-        tree.insert(7);
-        tree.insert(3);
-        tree.insert(9);
-        tree.insert(11);
-        tree.insert(17);
-        tree.insert(16);
-        tree.insert(19);
+        int[] list = {7,3,9,11,17,16,19};
+        for (int i : list) {
+            tree.insert(i);
+        }
         tree.traverseInOrder();
-        System.out.println("********************");
+        System.out.println("\n********************\n");
         tree.traversePreOrder();
-        System.out.println("********************");
+        System.out.println("\n********************\n");
         tree.traversePostOrder();
     }
 
+    // Test to verify an element is found in the tree if it exists
+    @Test
+    public void TestFindElement()
+    {
+        TreeNode tree = new TreeNode(15);
+        int[] list = {7,3,9,11,17,16,19};
+        for (int i : list) {
+            tree.insert(i);
+        }
+        boolean result = tree.findElement(17);
+        assertEquals(result,true);
+    }
+
+    // Test to verify an element is not found in the tree if it does not exist(Returns false)
+    @Test
+    public void TestFindElementThatDoesNotExist()
+    {
+        TreeNode tree = new TreeNode(15);
+        int[] list = {7,3,9,11,17,16,19};
+        for (int i : list) {
+            tree.insert(i);
+        }
+        boolean result = tree.findElement(28);
+        assertEquals(result,false);
+    }
 }
