a2a7ef22e5d510108d7c3a66262284cd318cbea5
Rahul <sinha.r@northeastern.edu>
2020-10-10 17:45:46 -0400

    Updated Tests

--- test/com/fundamentals/Tree/TreeNodeTest.java
+++ test/com/fundamentals/Tree/TreeNodeTest.java
@@ -1,6 +1,7 @@
 package com.fundamentals.Tree;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Array;
@@ -10,43 +11,57 @@ class TreeNodeTest {
 
     @Test
     //Test to create a tree and insert elements and do different types of traversals on the tree
-    public void TreeTest()
-    {
+    public void TreeTest() {
         TreeNode tree = new TreeNode(15);
-        int[] list = {7,3,9,11,17,16,19};
+        int[] list = {7, 3, 9, 11, 17, 16, 19};
         for (int i : list) {
             tree.insert(i);
         }
+        System.out.println("Inorder Traversal");
         tree.traverseInOrder();
         System.out.println("\n********************\n");
+        System.out.println("Preorder Traversal");
         tree.traversePreOrder();
         System.out.println("\n********************\n");
+        System.out.println("Postorder Traversal");
         tree.traversePostOrder();
     }
 
-    // Test to verify an element is found in the tree if it exists
+    // Test to verify an element larger than root node is found in the tree if it exists
     @Test
-    public void TestFindElement()
-    {
+    public void TestFindElement() {
         TreeNode tree = new TreeNode(15);
-        int[] list = {7,3,9,11,17,16,19};
+        int[] list = {7, 3, 9, 11, 17, 16, 19};
         for (int i : list) {
             tree.insert(i);
         }
         boolean result = tree.findElement(17);
-        assertEquals(result,true);
+        assertEquals(result, true);
+    }
+
+    // Test to verify an element smaleer than root node is found in the tree if it exists
+    @Test
+    public void TestFindElement2() {
+        TreeNode tree = new TreeNode(15);
+        int[] list = {7, 3, 9, 11, 17, 16, 19};
+        for (int i : list) {
+            tree.insert(i);
+        }
+        boolean result = tree.findElement(7);
+        assertEquals(result, true);
     }
 
     // Test to verify an element is not found in the tree if it does not exist(Returns false)
     @Test
-    public void TestFindElementThatDoesNotExist()
-    {
+    public void TestFindElementThatDoesNotExist() {
         TreeNode tree = new TreeNode(15);
-        int[] list = {7,3,9,11,17,16,19};
+        int[] list = {7, 3, 9, 11, 17, 16, 19};
         for (int i : list) {
             tree.insert(i);
         }
         boolean result = tree.findElement(28);
-        assertEquals(result,false);
+        assertEquals(result, false);
+        result = tree.findElement(2);
+        assertEquals(result, false);
     }
 }
